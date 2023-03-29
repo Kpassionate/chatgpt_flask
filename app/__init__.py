726167ba39ce666561d7f02a236fb10b7a08fdce
akai <guanyankai@qike366.com>
2023-03-29 11:52:18 +0800

    中间件/日志处理

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,42 @@
 #!/user/bin/python
 # _*_ coding:utf-8 _*_
+import os
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
-
 from app.api import api_v1
 from app.views import user
-from config import DevConfig
+from settings.config import DevConfig
 from app.models.base import db
+import time
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 def create_app():
     app = Flask(__name__)  # 实例化app
-
-    app.config.from_object(DevConfig)  # 加载配置(配置加载需要在初始化数据库之前，否则会报警告信息)
+    # 加载配置(配置加载需要在初始化数据库之前，否则会报警告信息)
+    app.config.from_object(DevConfig)
+    # jwt 配置
     jwt = JWTManager(app)
-    db.init_app(app)  # 初始化数据库
-
+    # 初始化数据库
+    db.init_app(app)
+    # 使用Migrate
     # manager = Manager(app)
     # migrate = Migrate(app, db)
     # manager.add_command('db', MigrateCommand)
     # db.create_all(app=app)  # 创建表结构
-    # app.register_blueprint(book.mod, url_prefix='/api/book')  # 注册book蓝图
-    app.register_blueprint(api_v1)  # 注册user蓝图
-
+    # 注册api_v1蓝图
+    app.register_blueprint(api_v1)
+    # 添加日志处理器
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    log_path = os.path.join(BASE_DIR, 'logs')
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    log_formatter = logging.Formatter('%(levelname)s %(asctime)s [%(module)s:%(funcName)s] %(message)s')
+    log_file = os.path.join(log_path, 'app-{}.log'.format(time.strftime('%Y-%m-%d')))
+    log_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024 * 10, backupCount=10)
+    log_handler.setFormatter(log_formatter)
+    log_handler.setLevel(logging.INFO)
+    app.logger.addHandler(log_handler)
     return app
-
--- manage.py
+++ manage.py
@@ -1,9 +1,47 @@
-from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
-
+import json
 from app import create_app
+from flask import request, jsonify
 
 app = create_app()
+
+
+# 中间件
+@app.before_request
+def get_request_info():
+    """请求"""
+    request_info = {
+        'method': request.method,
+        'url': request.url,
+        'params': request.json
+    }
+    app.logger.info(request_info)
+
+
+@app.errorhandler(Exception)
+def handle_value_error(e):
+    """异常"""
+    app.logger.error(str(e))
+    if hasattr(e, 'code'):
+        response = jsonify({'error': e.name + e.description})
+        response.status_code = e.code
+    else:
+        response = jsonify({'error': str(e)})
+        response.status_code = 500
+    return response
+
+
+@app.after_request
+def handle_response(response):
+    """响应"""
+    status = response.status_code
+    if status == 500:
+        response = jsonify({'code': '500', 'error': json.loads(response.data)})
+    elif status == 404:
+        response = jsonify({'code': '404', 'error': '请求路径不存在'})
+    elif status in [422, 405]:
+        response = jsonify({'code': '101', 'error': '请求参数错误'})
+    return response
+
 
 if __name__ == "__main__":
     app.run()
